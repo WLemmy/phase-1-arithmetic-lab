9a16bd906c6bd11b39c74e9fa5337ff92027880e
Wilfred Lemayian <wilfred.lemayian@gmail.com>
2024-03-23 14:31:04 +0300

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+const random = Math.floor(Math.random() * 100);
+
+const num3 = 14;
+const num4 = 5;
+
+const mod = (num3 % num4);
+console.log(mod);
+
+const numbers = [1,5,10,15,20];
+
+const max = Math.max(...numbers);
+console.log(max);
